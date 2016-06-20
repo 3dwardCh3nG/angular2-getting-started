41733fcc78dce331c5c48aa39ecdab37b70889fb
Edward Cheng <echeng@infomedia.com.au>
2016-06-20 14:27:29 +1000

    minor changes

--- app/app-component.ts
+++ app/app-component.ts
@@ -7,4 +7,25 @@ import {RandomColorDirective} from "./random-color-directive";
 })
 export class AppComponent {
     private title:string = "Angular 2.0 is in the house.";
+
+    constructor() {
+        this.test1();
+    }
+
+    test1() {
+        [1, 2, 3]
+            .map(item =>
+                `(${item})`)
+            .filter(item =>
+            item.length > 1)
+            .forEach(value =>
+                console.log(value));
+        const testObj = {a: 1, b: 2, c: 3};
+        const {a, b} = testObj;
+        this.test2(a, b, testObj);
+    }
+
+    test2(a = 1, b = 2, {c = 3}) {
+        console.log(a, b, c);
+    }
 }
